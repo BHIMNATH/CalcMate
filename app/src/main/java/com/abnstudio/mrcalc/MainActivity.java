1957775e08a15a94509af61888ea3359b7497a0f
Aswin Bhim Nath <aswismail@gmail.com>
2018-12-29 17:36:25 +0530

    Buttons UI

--- app/src/main/java/com/abnstudio/mrcalc/MainActivity.java
+++ app/src/main/java/com/abnstudio/mrcalc/MainActivity.java
@@ -10,7 +10,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     Button button0 , button1 , button2 , button3 , button4 , button5 , button6 ,
             button7 , button8 , button9 , buttonAdd , buttonSub , buttonDivision ,
-            buttonMul , buttonDot , buttonAC , buttonEqual, buttonBackspace, buttonAns, buttonSqrt ;
+            buttonMul , buttonDot , buttonAC , buttonEqual, buttonBackspace ;
+//    buttonAns, buttonSqrt ;
     TextView display ;
     float mValueOne, mValueTwo, lastAns = 0;
     boolean mAddition , mSubtract ,mMultiplication ,mDivision ;
